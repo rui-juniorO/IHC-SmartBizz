412b34cae182498185ba16bd76cd36e47042ae9f
Rui Delfim De Oliveira <rui.junior@ua.pt>
2023-04-29 09:09:15 -0700

    F

--- smart-biz/src/App.js
+++ smart-biz/src/App.js
@@ -5,12 +5,21 @@ import Main from './pages/main';
 
 function App() {
   return (
-    <>
-    
-    <TopNav /><SideNav />
-    <Main />
-    
-    </>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
