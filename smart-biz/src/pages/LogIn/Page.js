fa0631989e2bfc0534430b9ffc852368ce2a79e8
Rui Delfim De Oliveira <rui.junior@ua.pt>
2023-05-01 16:14:43 +0100

    Update Main

--- smart-biz/src/pages/LogIn/Page.js
+++ smart-biz/src/pages/LogIn/Page.js
@@ -1,8 +1,17 @@
 //Para propósitos de testes
 import React from "react";
+import SideNav from "../../components/nav-bar/nav";
+import TopNav from "../../components/top-nav/nav";
+import Main from "../main";
+
 function Page() {
 return (
-  <h1>Page</h1>
+  <>
+  <TopNav/>
+  <SideNav/>
+
+  <Main/>
+  </>
 );
 }
 
