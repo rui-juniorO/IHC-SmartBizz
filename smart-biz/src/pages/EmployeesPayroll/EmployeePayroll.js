fc92b47e6e7fb76ba4e23c8b6f536267a2a5bfd4
TK <kauati.toiti@gmail.com>
2023-05-12 15:05:08 +0100

    Selector ja funciona

--- smart-biz/src/pages/EmployeesPayroll/EmployeePayroll.js
+++ smart-biz/src/pages/EmployeesPayroll/EmployeePayroll.js
@@ -1,5 +1,6 @@
 import TopNav from '../../components/top-nav/nav'
 import Selector from './Selector'
+import employees from './Emps.json'
 import './Table.scss'
 //import Dropdown from 'react-bootstrap/Dropdown';
 import { useState } from 'react'
@@ -15,6 +16,7 @@ const EmployeePayroll = () => {
 
     const [tableGenerator, setTable] =  useState(false);
 
+    const [selectedDept, setDept] = useState("all");
 
     const states = [
         "contractDate", "incomeTaxes", "socialSecurity", 
@@ -26,6 +28,7 @@ const EmployeePayroll = () => {
         setRegularHours, setExtraHours
     ]
 
+
     /*
     useEffect(() => {
             // Iterar sobre os estados e armazenar no localStorage se o valor for verdadeiro
@@ -36,7 +39,46 @@ const EmployeePayroll = () => {
             });
         }, [states]);
     */
-
+    
+    const checkEmpDept = (emp) => {
+        //Default : todos os departamentos
+        if(selectedDept === null || selectedDept === "all"){
+            return (
+                <tr>
+                    <td>{emp['ID']}</td>
+                    <td>{emp['Name']}</td>
+                    <td>{emp['Department']}</td>
+                    <td>{emp['Gross Salary']}</td>
+                    <td>{emp['Liquid Salary']}</td>
+                    { contractDate && <td>{emp['Contract Date']}</td>}
+                    { incomeTaxes && <td>{emp['Income Taxes Deduciton']}</td>}
+                    { socialSecurity && <td>{emp['Social Security']}</td>}
+                    { regularHours&& <td>{emp['Regular Hours of work']}</td>}
+                    { extraHours&& <td>{emp['Extra Hours of work']}</td>}
+                                
+                </tr>
+            );
+        }
+        //O Departamento selecionado no select
+        else if(emp['Department'] == selectedDept){
+            return (
+                <tr>
+                    <td>{emp['ID']}</td>
+                    <td>{emp['Name']}</td>
+                    <td>{emp['Department']}</td>
+                    <td>{emp['Gross Salary']}</td>
+                    <td>{emp['Liquid Salary']}</td>
+                    { contractDate && <td>{emp['Contract Date']}</td>}
+                    { incomeTaxes && <td>{emp['Income Taxes Deduciton']}</td>}
+                    { socialSecurity && <td>{emp['Social Security']}</td>}
+                    { regularHours&& <td>{emp['Regular Hours of work']}</td>}
+                    { extraHours&& <td>{emp['Extra Hours of work']}</td>}
+                                
+                </tr>
+            );
+        }
+        else {return null;}
+    }
     
     const storeState = (key ,state) => {
         localStorage.setItem(key, state);
@@ -44,7 +86,9 @@ const EmployeePayroll = () => {
 
 
 
+
     const tableTrigger = () => {
+        console.log("Dept : ", selectedDept);
         if(tableGenerator === false){
             console.log(tableGenerator)
             setTable(true);
@@ -158,8 +202,9 @@ const EmployeePayroll = () => {
                         <table className='responstable'>
                         <thead>
                             <tr>
-                                <th>Name</th>
                                 <th>ID</th>
+                                <th>Name</th>
+                                <th>Department</th>
                                 <th>Gross Salary</th>
                                 <th>Liquid Salary</th>
                                 { contractDate && <th>Contract Date</th>}
@@ -172,17 +217,54 @@ const EmployeePayroll = () => {
 
 
                         <tbody>
-                            <tr>
-                                <td>Name</td>
-                                <td>ID</td>
-                                <td>Gross Salary</td>
-                                <td>Liquid Salary</td>
-                                { contractDate && <td>Contract Date</td>}
-                                { incomeTaxes && <td>Income Taxes Deduction</td>}
-                                { socialSecurity && <td>Social Security</td>}
-                                { regularHours&& <td>Regular Hours of Work</td>}
-                                { extraHours&& <td>Extra Hours of Work</td>}
-                            </tr>
+
+                            {
+                                employees.employees.map((emp, idx) => {
+                                    {
+                                        if(selectedDept == null || selectedDept == "all"){
+                                            return (
+                                                <tr key={idx}>
+                                                     <td>{emp['ID']}</td>
+                                                    <td>{emp['Name']}</td>
+                                                    <td>{emp['Department']}</td>
+                                                    <td>{emp['Gross Salary']}</td>
+                                                    <td>{emp['Liquid Salary']}</td>
+                                                    { contractDate && <td>{emp['Contract Date']}</td>}
+                                                    { incomeTaxes && <td>{emp['Income Taxes Deduciton']}</td>}
+                                                    { socialSecurity && <td>{emp['Social Security']}</td>}
+                                                    { regularHours&& <td>{emp['Regular Hours of work']}</td>}
+                                                    { extraHours&& <td>{emp['Extra Hours of work']}</td>}
+                                                </tr>
+
+
+                                            );
+                                        }
+                                        else if (emp['Department'] == selectedDept){
+                                            return (
+                                                <tr key={idx}>
+                                                     <td>{emp['ID']}</td>
+                                                    <td>{emp['Name']}</td>
+                                                    <td>{emp['Department']}</td>
+                                                    <td>{emp['Gross Salary']}</td>
+                                                    <td>{emp['Liquid Salary']}</td>
+                                                    { contractDate && <td>{emp['Contract Date']}</td>}
+                                                    { incomeTaxes && <td>{emp['Income Taxes Deduciton']}</td>}
+                                                    { socialSecurity && <td>{emp['Social Security']}</td>}
+                                                    { regularHours&& <td>{emp['Regular Hours of work']}</td>}
+                                                    { extraHours&& <td>{emp['Extra Hours of work']}</td>}
+                                                </tr>
+
+
+                                            );
+                                        }
+                                        else {return null;}
+                                    }
+                                   
+                                }               
+                                                    )
+                            
+                            }
+                            
                         </tbody>
                     </table>
                     )
@@ -247,10 +329,10 @@ const EmployeePayroll = () => {
                 <br></br>
                 <br></br>
                 <h4>Department : </h4>
-                <h5>By default its queries all the departments</h5>
+                <h5>By default queries all departments</h5>
                 <br></br>
                 <br></br>
-                <Selector></Selector>
+                <Selector selection={setDept} opt={selectedDept}></Selector>
 
                 <br></br>
                 <br></br>
--- smart-biz/src/pages/EmployeesPayroll/Selector.js
+++ smart-biz/src/pages/EmployeesPayroll/Selector.js
@@ -1,6 +1,8 @@
 import Select from 'react-select'
+import React from 'react'
 import './Style.css'
-const Selector = () => {
+const Selector = (props) => {
+
     const options = [
         { value: 'all', label: 'all' },
         { value: 'Sales', label: 'Sales' },
@@ -9,11 +11,21 @@ const Selector = () => {
         { value: 'Human Resources', label: 'Human Resources' },
         { value: 'Logistics', label: 'Logistics' },
         { value: 'Directory', label: 'Directory' }
-      ]
-    return <Select options={options}  required
-    className={{
-        innerHeight : '30px'
-    }}/>
+      ];
+
+      const handleSelectChange = (selectedOption) => {
+        props.selection(selectedOption['value']); // Chama a função de seleção passada por props
+        //console.log("Dept : ", selectedOption['value'])
+    };
+
+
+    return (<Select 
+        value={props.opt}
+        onChange={handleSelectChange}
+        options={options}
+        className={{
+            innerHeight : '30px'
+        }}/>);
 }
 
 export default Selector;
