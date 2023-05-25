34e2070bbc5d41463e884368418a3d6744986a73
TK <kauati.toiti@gmail.com>
2023-05-25 17:09:41 +0100

    Ajustes (quase) finais 3

--- smart-biz/src/components/Exportation/ExportOpts.js
+++ smart-biz/src/components/Exportation/ExportOpts.js
@@ -5,11 +5,14 @@ import csv from '../../assets/csv-icon.svg'
 import xls from '../../assets/xls-icon.svg'
 function ExportOpts(){
     return  (
-        <div className="OptsGrid">
-            <div> <img src={pdf} ></img> </div> 
-            <div> <img src={jpg}></img> </div> 
-            <div> <img src={csv}></img> </div> 
-            <div> <img src={xls}></img> </div> 
+        <div>
+            <h4 style={{ padding : "5px" }}>Export as</h4>
+            <div className="OptsGrid">
+                <div> <img src={pdf} ></img> </div> 
+                <div> <img src={jpg}></img> </div> 
+                <div> <img src={csv}></img> </div> 
+                <div> <img src={xls}></img> </div> 
+            </div>
         </div>
     )
 }
